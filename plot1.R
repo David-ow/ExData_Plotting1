49f4ecc4b5dbb65b9599cfc8dc5f868730856e4f
David Ow <mmotl86@gmail.com>
2014-06-04 17:21:29 +0800

    Plots and scripts for assignment

---
+++ plot1.R
@@ -0,0 +1,8 @@
+library(data.table)
+fulldata <- fread("./household_power_consumption.txt")
+fulldata$Date <- as.Date(fulldata$Date, format="%d/%m/%Y")
+select.data <- subset(fulldata, Date=="2007-02-01" | Date=="2007-02-02")
+select.data$Global_active_power <- as.numeric(select.data$Global_active_power)
+hist(select.data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)",col="red")
+dev.copy(png, file="plot1.png")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,10 @@
+library(data.table)
+fulldata <- fread("./household_power_consumption.txt")
+fulldata$Date <- as.Date(fulldata$Date, format="%d/%m/%Y")
+select.data <- subset(fulldata, Date=="2007-02-01" | Date=="2007-02-02")
+select.data$Global_active_power <- as.numeric(select.data$Global_active_power)
+select.data[,date.time:=paste(as.character(Date),Time)]
+formatted.time <- strptime(select.data$date.time, format="%Y-%m-%d %H:%M:%S")
+plot(formatted.time,select.data$Global_active_power, type="l", xlab="", ylab="Global active power (kilowatts)")
+dev.copy(png, file="plot2.png")
+dev.off()
